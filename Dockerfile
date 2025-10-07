10e470bbacd29db0235b542b47d5d211853db0ee
asaba <41068364+asaba-zauberer@users.noreply.github.com>
2025-10-07 13:11:52 +0900

    change port

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-EXPOSE 8000
+EXPOSE 9999
 
-CMD ["gunicorn", "-b", "0.0.0.0:8000", "app:create_app()"]
+CMD ["gunicorn", "-b", "0.0.0.0:9999", "app:create_app()"]
--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
+
 from server.app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    port = int(os.environ.get("PORT", "9999"))
+    app.run(host="0.0.0.0", port=port, debug=True)
